86e34aedfe0dd071385e094b7026cd0ad14b522c
Ian-Rojas <joshue3345@gmail.com>
2026-05-07 15:45:32 -0400

    Repaso

---
+++ 07-05.py
@@ -0,0 +1,57 @@
+import random, time
+# n1 = int(input("Ingrese el valor del limite inferior: "))
+# n2 = int(input("Ingrese el valor del limite superior: "))
+
+# while n2<=n1:
+#     print("Error, el limite superior debe ser mayor.")
+#     n2 = int(input("Ingrese el valor del limite superior: "))
+# num=random.randint(n1,n2)
+# print(num)
+'''
+Realizar las calsificacion de peces 
+generar una cantidad aleatorea de captura de peces entre 10 y 20
+capturar peces y clasificarlos por su peso
+para saber como se venderan
+800 grs o menos, a lata
+801 o más a la plancha (max 3000)
+contar cuantos quedaron a la plancha y 
+cuantos quedaron para embasar en lata
+'''
+
+# pez = random.randint(10, 20)
+# lata = 0
+# plancha = 0
+# print(f"Se capturaron {pez} peces!!")
+# for i in range(pez):
+#     peso = random.randint(500, 3000)
+#     if peso <= 800:
+#         lata += 1
+#     else:
+#         plancha += 1
+# print(f"Los peces que quedaron en plancha son {plancha}.")
+# print(f"Los peces que quedaron para embasar en lata son {lata}")
+
+peso = abs(int(input("Ingrese el peso del producto: ")))
+sodio = int(input("Ingrese el porcentaje de sodio: "))
+mercado = int(input("Ingrese el mercado del producto: 1. nacional, 2. internacional: "))
+
+if peso <= 500:
+    lata = "lata normal"
+elif 500 < peso <= 1500:
+    lata = "lata mediana"
+else:
+    lata = "lata grande"
+
+if sodio < 5:
+    sod = ""
+elif 5 <= sodio <= 8:
+    sod = "lata especial"
+else:
+    sod = "acorazada"
+
+if mercado == 1:
+    sticker = ""
+else:
+    sticker = "con sticker sanitario"
+
+print(f"{lata} {sod} {sticker}")
